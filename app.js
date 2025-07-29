55fd5e5d8042412b0a603acbc44c523ba8a3c75d
Balram72 <balrampanda72@gmail.com>
2025-07-29 20:55:00 +0530

    use the minify,compression,node-cache

--- app.js
+++ app.js
@@ -6,15 +6,44 @@ const experessLayouts = require("express-ejs-layouts");
 const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const expressEjsLayouts = require("express-ejs-layouts");
+const minifyHTML = require("express-minify-html-terser");
+var compression = require("compression");
+
 require("dotenv").config();
 
 // Middleware
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public"), { maxAge: "1d" })); //Use user side Cacheing -> { maxAge: "1d" }
 app.use(cookieParser());
 app.use(experessLayouts);
+app.use(
+  compression({
+    level: 9,
+    threshold: 10 * 1024,
+    filter: (req, res) => {
+      if (req.headers["x-no-compression"]) {
+        return false;
+      }
+      return compression.filter(req, res);
+    },
+  })
+); // use the compression all data
 
+// use the minifyHTML middlewarecls
+app.use(
+  minifyHTML({
+    override: true,
+    htmlMinifier: {
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: true,
+      removeAttributeQuotes: true,
+      removeEmptyAttributes: true,
+      minifyJS: true,
+    },
+  })
+);
 // view Engine
 app.set("view engine", "ejs");
 
--- middleware/loadCommonData.js
+++ middleware/loadCommonData.js
@@ -1,21 +1,44 @@
 const newsModel = require("../models/News");
 const categoryModel = require("../models/Category");
 const settingModel = require("../models/Setting");
+// use the Server Side Cacheing
+// cache.set('key',value,time)->Set cache
+// cache.get('key')->Get cache
+// cache.del('key')->delete cache
+// cache.flushAll->get all cache
+// cache.keys()->show all cache key name
+
+const NodeCache = require("node-cache");
+const cache = new NodeCache();
 
 const loadCommonData = async (req, res, next) => {
   try {
-    const setting = await settingModel.findOne();
-    const latestNews = await newsModel
-      .find()
-      .populate("category", { name: 1, slug: 1 })
-      .populate("author", "fullname")
-      .sort({ createAt: -1 })
-      .limit(5);
+    var latestNews = cache.get("latestNewsCache");
+    var categories = cache.get("categoriesCache");
+    var setting = cache.get("settingCache");
 
-    const categoryInUse = await newsModel.distinct("category");
-    const categories = await categoryModel.find({
-      _id: { $in: categoryInUse },
-    });
+    if (!latestNews && !categories && !setting) {
+      setting = await settingModel.findOne().lean();
+      latestNews = await newsModel
+        .find()
+        .populate("category", { name: 1, slug: 1 })
+        .populate("author", "fullname")
+        .sort({ createAt: -1 })
+        .limit(5)
+        .lean(); // useing the java script object
+
+      const categoryInUse = await newsModel.distinct("category");
+      categories = await categoryModel
+        .find({
+          _id: { $in: categoryInUse },
+        })
+        .lean();
+
+      cache.set("latestNewsCache", latestNews, 60 * 60);
+      cache.set("categoriesCache", categories, 3600);
+      cache.set("settingCache", setting, 3600);
+    }
+
     res.locals.setting = setting;
     res.locals.latestNews = latestNews;
     res.locals.categories = categories;
